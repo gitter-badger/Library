d3b467aa33f7dc087fe99d61576a5b026693535c
SetSplin <igortarakanov144999usa@gmail.com>
2015-08-02 22:25:15 +0300

    [+}] вкладки для терминала

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 user = {"name": "Иван Иванов", "booksCount": 12}
+devise = "terminal"
 
 @app.route("/login")
 def login():
@@ -15,14 +16,22 @@ def reg():
 
 @app.route("/")
 def home():
-	return render_template("home.html", title = "Главная", user = user)
+	return render_template("home.html", title = "Главная", user = user, devise = devise)
 
 @app.route("/myBooks")
 def myBooks():
-	return render_template("myBooks.html", title = "Мои книги", user = user)
+	return render_template("myBooks.html", title = "Мои книги", user = user, devise = devise)
 
 @app.route("/books")
 def books():
-	return render_template("books.html", title = "Каталог", user = user)
+	return render_template("books.html", title = "Каталог", user = user, devise = devise)
+
+@app.route("/handBooks")
+def handBooks():
+	return render_template("handBooks.html", title = "Взять книги", user = user, devise = devise)
+
+@app.route("/returnBooks")
+def returnBooks():
+	return render_template("returnBooks.html", title = "Вернуть книги", user = user, devise = devise)
 
 app.run(host="::", port=5000, debug=True)
