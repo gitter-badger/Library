517905b26571916944632f8ed42ed58796787c62
SetSplin <igortarakanov144999usa@gmail.com>
2015-08-03 23:43:25 +0300

    [+] mongo

--- demon.py
+++ demon.py
@@ -8,11 +8,11 @@ config.read('config.cf')
 
 def functionUser():
 	devise = open(config["Demon"]["userScanner"])
-	requests.post(url = "localhost:5000", data = {"type": "user", "data": devise.readline().strip("\2\3\r\n")})
+	requests.post(url = "http://localhost:5000/term", data = {"type": "user", "data": devise.readline().strip("\2\3\r\n"), "id": "testTerminal"})
 
 def functionBook():
 	devise = open(config["Demon"]["bookScanner"])
-	requests.post(url = "localhost:5000", data = {"type": "book", "data": devise.readline().strip("\2\3\r\n")})
+	requests.post(url = "http://localhost:5000/term", data = {"type": "book", "data": devise.readline().strip("\2\3\r\n"), "id": "testTerminal"})
 
 threadUser = threading.Thread(target = functionUser)
 threadBook = threading.Thread(target = functionBook)
--- server.py
+++ server.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, redirect
-import configparser
+from flask import Flask, render_template, redirect, request
+import configparser, pymongo
 
 config = configparser.ConfigParser()
 config.read('config.cf')
 
+base = pymongo.MongoClient()
+users = base.digital_library.users
+books = base.digital_library.books
+hands = base.digital_library.hands
+tags = base.digital_library.tags
+
 app = Flask(__name__)
 
-user = {"name": "Иван Иванов", "booksCount": 4}
+user = {"name": "Иван Иванов"}
 devise = "terminal"
 booksList = [
 	{
@@ -35,6 +41,7 @@ booksList = [
 ]
 
 recBooksList = booksList
+booksLen = len(booksList)
 
 @app.route("/login")
 def login():
@@ -46,26 +53,80 @@ def reg():
 
 @app.route("/")
 def home():
-	return render_template("home.html", title = "Главная", user = user, devise = devise, recBooks = recBooksList)
+	return render_template(
+		"home.html",
+		title = "Главная", 
+		user = user, 
+		devise = devise, 
+		recBooks = recBooksList, 
+		books = booksList, 
+		booksLen = booksLen
+		)
 
-@app.route("/myBooks")
-def myBooks():
-	return render_template("myBooks.html", title = "Мои книги", user = user, devise = devise, books = booksList)
+@app.route("/handed")
+def handed():
+	return render_template(
+		"handed.html", 
+		title = "Мои книги", 
+		user = user, 
+		devise = devise, 
+		books = booksList, 
+		booksLen = booksLen
+		)
 
 @app.route("/books")
 def books():
-	return render_template("books.html", title = "Каталог", user = user, devise = devise)
+	return render_template(
+		"books.html", 
+		title = "Каталог", 
+		user = user, 
+		devise = devise, 
+		books = booksList, 
+		booksLen = booksLen
+		)
 
-@app.route("/handBooks")
-def handBooks():
-	return render_template("handBooks.html", title = "Взять книги", user = user, devise = devise)
-
-@app.route("/returnBooks")
-def returnBooks():
-	return render_template("returnBooks.html", title = "Вернуть книги", user = user, devise = devise)
+@app.route("/operations")
+def operations():
+	return render_template(
+		"operations.html", 
+		title = "Взять/Вернуть книги", 
+		user = user, 
+		devise = devise, 
+		books = booksList, 
+		booksLen = booksLen
+		)
 
 @app.route('/term', methods=['POST'])
 def tarminal():
+	form = request.form
+	if(form["type"] == "user"):
+		newTerminalUser(form["id"], form["data"])
+		return ""
+	else:
+		userBookOperation(form["id"], form["data"])
+		return ""
+
+def newTerminalUser(terminal, user):
 	pass
+
+def curentTerminalUser(terminal):
+	return "testUser"
+
+def successBookHand(terminal, book):
+	pass
+
+def successBookReturn(terminal, book):
+	pass
+
+def userBookOperation(terminal, book):
+	global hands, books
+	if(list(hands.find({"user": curentTerminalUser(terminal)})) == []):
+		hands.insert({"user": curentTerminalUser(terminal), "book": book, "time": time.ctime(time.time()), "status": "on"})
+		books.update({"_id": book}, {"$set": {"handed": int(books.find_one({"_id": book_id})["handed"]) + 1}})
+		successBookHand(terminal, book)
+	else:
+		hands.update({"user": curentTerminalUser(terminal), "book": book}, {"$set": {"status": "off"}})
+		books.update({"_id": book}, {"$set": {"handed": int(books.find_one({"_id": book_id})["handed"]) - 1}})
+		successBookReturn(terminal, book)
 
 app.run(host = config["Server"]["host"], port = 5000, debug = True)
