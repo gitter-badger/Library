46401a49e25d0a14c16594a4111959e8ebf6abfd
SetSplin <igortarakanov144999usa@gmail.com>
2015-08-03 17:09:00 +0300

    [+] request.post

--- demon.py
+++ demon.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import threading, time
-
-def userToServer(data):
-	pass
-
-def userToServer(data):
-	pass
+import threading, time, requests
 
 def functionUser():
 	devise = open("path")
-	data = devise.readline().strip("\2\3\r\n")
-	userToServer(data)
+	requests.post(url = "localhost:5000", data = {"type": "user", "data": devise.readline().strip("\2\3\r\n")})
 
 def functionBook():
 	devise = open("path")
-	data = devise.readline().strip("\2\3\r\n")
-	bookToServer(data)
+	requests.post(url = "localhost:5000", data = {"type": "book", "data": devise.readline().strip("\2\3\r\n")})
 
 threadUser = threading.Thread(target = functionUser)
 threadBook = threading.Thread(target = functionBook)
